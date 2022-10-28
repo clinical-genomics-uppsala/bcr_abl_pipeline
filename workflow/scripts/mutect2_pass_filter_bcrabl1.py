b63e1b2f72006debea0422c035c4dd58fc06c108
Arielle R Munters <arielle@reivant.se>
2022-10-28 11:58:14 +0200

    ci: include pisces in config files and remove old files

--- workflow/scripts/mutect2_pass_filter_bcrabl1.py
+++
@@ -1,9 +0,0 @@
-import sys
-from pysam import VariantFile
-
-vcf_in = VariantFile(snakemake.input.vcf)
-vcf_out = VariantFile(snakemake.output.vcf, 'w', header=vcf_in.header)
-
-for record in vcf_in.fetch():
-    if record.filter.keys() == ["PASS"] or record.filter.keys() == ["multiallelic"] or record.filter.keys() == ["strand_bias"] or record.filter.keys() == ["haplotype"] or ("strand_bias" in list(record.filter) and "haplotype" in list(record.filter)):
-        vcf_out.write(record)
