502edc5e0d56a53bde5e9b294e3fabcc0c177fea
Arielle R Munters <arielle@reivant.se>
2024-03-21 14:38:01 +0100

    style: format python

--- workflow/scripts/summary_report.py
+++ workflow/scripts/summary_report.py
@@ -9,11 +9,11 @@ vcf = VariantFile(snakemake.input.vcf)
 
 sample = str(vcf.header.samples[0])
 today = date.today()
-emptyList = ['', '', '', '', '', '']
+emptyList = ["", "", "", "", "", ""]
 
 
 depth_dict = {}
-with gzip.open(snakemake.input.mosdepth_regions, 'rt') as mosdepthfile:
+with gzip.open(snakemake.input.mosdepth_regions, "rt") as mosdepthfile:
     for lline in mosdepthfile:
         line = lline.strip().split("\t")
         depth_dict[line[3]] = int(float(line[4]))
@@ -27,7 +27,7 @@ with open(snakemake.input.background, "r") as backgroundfile:
 # All positions in Branford list
 # Add vaiant change in bedfile chr, pos, mutation name, , ref, alt, c-name
 branfordVariants = []
-with open(snakemake.input.branford, 'r') as branfordfile:
+with open(snakemake.input.branford, "r") as branfordfile:
     for line in branfordfile:
         branfordVariants.append(line.strip().split("\t"))
 
@@ -62,7 +62,7 @@ allHeader = [
 
 for record in vcf.fetch():
     if record.filter.keys() == ["PASS"]:
-        filter = ''
+        filter = ""
     else:
         filter = ", ".join(list(record.filter))
 
@@ -93,7 +93,7 @@ for record in vcf.fetch():
             dp = str(record.info["DP"])
             index = list(record.alts).index(b_line[5])
             af = str(round(record.info["AF"][index], 3))
-            a_dp = str(record.samples[sample].get("AD")[index+1])
+            a_dp = str(record.samples[sample].get("AD")[index + 1])
             outline = [b_line[6], b_line[3], b_line[0], str(b_line[2]), b_line[4], b_line[5], af, a_dp, dp]
             branfordSNV.append(outline)
             break
@@ -104,11 +104,33 @@ for b_line in branfordVariants:
     if ordered_b_line not in [outlines[0:6] for outlines in branfordSNV]:
         branfordSNV.append(ordered_b_line + ["", "", str(depth_dict[ordered_b_line[1]])])
 
+# Add background median for each bradford position
+i = 0
+for bradford_line in branfordSNV:
+    for line in background_table:
+        if line[1] == bradford_line[3]:
+            background_median = line[2]
+            break
+    branfordSNV[i].append(background_median)
+    i += 1
+
 # Add fusions from Arriba
 fusion_lines = []
-fusionHeading = ["Gene1", "Gene2", "Gene1_breakpoint", "Gene2_breakpoint", "BCR-ABL1 Type", "Fusion Type", "Split reads1",
-                 "Split reads2", "Discordant Mates", "Coverage gene1", "Coverage gene2", "Confidence"]
-with open(snakemake.input.arriba_tsv, 'r') as tsv_arriba:
+fusionHeading = [
+    "Gene1",
+    "Gene2",
+    "Gene1_breakpoint",
+    "Gene2_breakpoint",
+    "BCR-ABL1 Type",
+    "Fusion Type",
+    "Split reads1",
+    "Split reads2",
+    "Discordant Mates",
+    "Coverage gene1",
+    "Coverage gene2",
+    "Confidence",
+]
+with open(snakemake.input.arriba_tsv, "r") as tsv_arriba:
     headerIndex = tsv_arriba.readline().strip().split("\t")
     for lline in tsv_arriba:
         line = lline.strip().split("\t")
@@ -124,11 +146,20 @@ with open(snakemake.input.arriba_tsv, 'r') as tsv_arriba:
                 elif int(gene1_pos.split(":")[1]) >= 23651611:
                     fusion_type = " Micro"
 
-            outline = [line[headerIndex.index("#gene1")], line[headerIndex.index("gene2")], gene1_pos, gene2_pos, fusion_type,
-                       line[headerIndex.index("type")], line[headerIndex.index("split_reads1")],
-                       line[headerIndex.index("split_reads2")], line[headerIndex.index("discordant_mates")],
-                       line[headerIndex.index("coverage1")], line[headerIndex.index("coverage2")],
-                       line[headerIndex.index("confidence")]]
+            outline = [
+                line[headerIndex.index("#gene1")],
+                line[headerIndex.index("gene2")],
+                gene1_pos,
+                gene2_pos,
+                fusion_type,
+                line[headerIndex.index("type")],
+                line[headerIndex.index("split_reads1")],
+                line[headerIndex.index("split_reads2")],
+                line[headerIndex.index("discordant_mates")],
+                line[headerIndex.index("coverage1")],
+                line[headerIndex.index("coverage2")],
+                line[headerIndex.index("confidence")],
+            ]
             fusion_lines.append(outline)
 
 
@@ -139,17 +170,17 @@ worksheetBran = workbook.add_worksheet("Branford variants")
 worksheetSNV = workbook.add_worksheet("SNV variants")
 worksheetFusion = workbook.add_worksheet("Fusion")
 
-headingFormat = workbook.add_format({'bold': True, 'font_size': 18})
-lineFormat = workbook.add_format({'top': 1})
-tableHeadFormat = workbook.add_format({'bold': True, 'text_wrap': True})
-textwrapFormat = workbook.add_format({'text_wrap': True})
-italicFormat = workbook.add_format({'italic': True})
-boldFormat = workbook.add_format({'bold': True})
-redFormat = workbook.add_format({'font_color': 'red'})
+headingFormat = workbook.add_format({"bold": True, "font_size": 18})
+lineFormat = workbook.add_format({"top": 1})
+tableHeadFormat = workbook.add_format({"bold": True, "text_wrap": True})
+textwrapFormat = workbook.add_format({"text_wrap": True})
+italicFormat = workbook.add_format({"italic": True})
+boldFormat = workbook.add_format({"bold": True})
+redFormat = workbook.add_format({"font_color": "red"})
 
-''' Overview sheet '''
+""" Overview sheet """
 worksheetOver.write(0, 0, sample, headingFormat)
-worksheetOver.write(1, 0, "Processing date: "+today.strftime("%B %d, %Y"))
+worksheetOver.write(1, 0, "Processing date: " + today.strftime("%B %d, %Y"))
 worksheetOver.write_row(2, 0, emptyList, lineFormat)
 
 worksheetOver.write(3, 0, "Created by: ")
@@ -169,7 +200,7 @@ worksheetOver.write(15, 0, "Bedfile used for variantcalling: " + str(snakemake.i
 worksheetOver.write(16, 0, "Backgroundfile used: " + str(snakemake.input.background))
 
 
-''' Branford list sheet '''
+""" Branford list sheet """
 worksheetBran.write("A1", "Branford variants found", headingFormat)
 worksheetBran.write("A3", "Sample: " + str(sample))
 worksheetBran.write("A4", "Branford file: " + str(snakemake.input.branford))
@@ -185,7 +216,7 @@ for line in branfordSNV:
     row += 1
 
 
-''' SNV ABL1 variants sheet '''
+""" SNV ABL1 variants sheet """
 worksheetSNV.write("A1", "Variants found in ABL1 or BCR", headingFormat)
 worksheetSNV.write("A3", "Sample: " + str(sample))
 worksheetSNV.write("A4", "Bedfile used: " + str(snakemake.input.bed))
@@ -198,14 +229,14 @@ for line in allSNVs:
     worksheetSNV.write_row(row, col, line)
     row += 1
 
-''' Fusion sheet '''
-worksheetFusion.set_column('C:D', 15)
-worksheetFusion.set_column('F:F', 15)
+""" Fusion sheet """
+worksheetFusion.set_column("C:D", 15)
+worksheetFusion.set_column("F:F", 15)
 worksheetFusion.write("A1", "Fusions detected with Arriba", headingFormat)
 worksheetFusion.write("A3", "Sample: " + str(sample))
 worksheetFusion.write("A5", "Primary fusion call from Arriba")
 
-worksheetFusion.insert_image("A6", snakemake.input.jpg, {'x_scale': 0.25, 'y_scale': 0.25})
+worksheetFusion.insert_image("A6", snakemake.input.jpg, {"x_scale": 0.25, "y_scale": 0.25})
 worksheetFusion.write_row("A40", fusionHeading, tableHeadFormat)
 row = 40
 for line in fusion_lines:
